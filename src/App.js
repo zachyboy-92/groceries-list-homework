546ddacc978cfd359bf927ee48af244f7759c203
zachyboy-92 <zakaria_0456@hotmail.com>
2021-09-04 20:16:18 -0400

    new repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from "react";
 import "./App.css";
-import GroceriesListForm from "./component/GroceriesListForm";
+import Groceries from "./component/Groceries";
+import NewGroceriesList from "./component/NewGroceriesList";
+
+const dummyGroceriesList = [
+  {
+    id: 1,
+    item: "milk",
+    units: "2 gallons",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    item: "eggs",
+    units: "12 pack",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    item: "bread",
+    units: "1 pack",
+    quantity: 1,
+  },
+];
 
 function App() {
+  const [groceries, setGroceries] = useState(dummyGroceriesList);
+
+  const addGroceriesHandler = (grocerie) => {
+    setGroceries((prevGroceries) => {
+      return [grocerie, ...prevGroceries];
+    });
+  };
+
   return (
     <div className="App">
       <h1>Groceries List</h1>
-      <GroceriesListForm />
+      <NewGroceriesList onAddGroceries={addGroceriesHandler} />
+      <Groceries groceriesList={groceries} />
     </div>
   );
 }
